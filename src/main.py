65200e2f88c352bd3cdd083dabf8edffaa6251ee
Emma-Nuell <okoyemcpaul86@gmail.com>
2025-09-19 22:47:24 +0100

    Fix pipenv Python version issue in CI

--- src/main.py
+++ src/main.py
@@ -1,20 +1,11 @@
-import argparse
 from .app import DrawingApp
-import sys
-import pytest
+
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--test", action="store_true", help="Run test suite instead of app")
-    args = parser.parse_args()
-
-    if args.test:
-        # run pytest programmatically, exit with the right status code
-        sys.exit(pytest.main(["-v", "src/tests"]))
-    else:
-        app = DrawingApp()
-        app.mainloop()
+    """Main function"""
+    app = DrawingApp()
+    app.mainloop()
 
 if __name__ == "__main__":
     main()
